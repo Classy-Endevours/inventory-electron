7d71d1df481de0c3d17a3a96ec08a63f8c93ab24
Manthan Mehta <>
2021-05-08 01:53:38 +0530

    Item migration and database functions completed.

---
+++ controller/itemsController.js
@@ -0,0 +1,69 @@
+const { ipcMain } = require('electron');
+const isDev = require('electron-is-dev');
+
+const response = isDev
+  ? require('../config/responseConfig')
+  : require(`${path.join(__dirname, '', '../config/responseConfig')}`);
+const items = isDev
+  ? require('../models').items
+  : require(`${path.join(__dirname, '', '../models')}`).items;
+
+ipcMain.on('items-create-message', async (event, arg) => {
+  try {
+    const item = await items.create(arg);
+    if (item !== null) {
+      event.reply(
+        'items-create-reply',
+        response.success('Item Saved successfully', item),
+      );
+    } else {
+      const err = new Error('Item saving failed');
+      throw err;
+    }
+  } catch (error) {
+    event.reply('items-create-reply', response.error(error.message));
+  }
+});
+
+ipcMain.on('items-fetch-message', async (event, arg) => {
+  try {
+    const options = {};
+    if (arg.where) options.where = arg.where;
+    if (arg.offset) {
+      options.offset = arg.offset;
+      options.limit = 15;
+    }
+    const item = await items.findAll(options);
+    if (item.length > 0) {
+      event.reply(
+        'items-fetch-reply',
+        response.success('Items Found successfully', item),
+      );
+    } else {
+      const err = new Error('No Items Found');
+      throw err;
+    }
+  } catch (error) {
+    event.reply('items-fetch-reply', response.error(error.message));
+  }
+});
+
+ipcMain.on('items-update-message', async (event, arg) => {
+  try {
+    const options = {};
+    if (arg.where) options.where = arg.where;
+
+    const item = await items.update(arg.values, options);
+    if (item.length > 0) {
+      event.reply(
+        'items-update-reply',
+        response.success('Items Updated successfully', item),
+      );
+    } else {
+      const err = new Error('No Items Updated');
+      throw err;
+    }
+  } catch (error) {
+    event.reply('items-update-reply', response.error(error.message));
+  }
+});
---
+++ migrations/20210507191820-create-items.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      productName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      composition: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      percent: {
+        allowNull: false,
+        type: Sequelize.FLOAT,
+      },
+      hsnCode: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      isDeleted: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('items');
+  },
+};
