8fa93ee8fc78a114c7173f66b72897bcfe2f55c1
Manthan Mehta <>
2021-05-22 02:22:17 +0530

    Migration Bug Solved

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
 const fs = require('fs');
 const path = require('path');
 const { Sequelize } = require('sequelize');
@@ -20,66 +22,30 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config);
 }
-const migrations = new Umzug({
-  migrations: {
-    // indicates the folder containing the migration .js files
-    path: path.join(__dirname, '../migrations'),
-    // inject sequelize's QueryInterface in the migrations
-    params: [sequelize.getQueryInterface()],
-  },
-  storage: 'sequelize',
-  storageOptions: {
-    sequelize, // here should be a sequelize instance, not the Sequelize module
-  },
-  logger: console,
-});
-const seeders = new Umzug({
-  migrations: {
-    // indicates the folder containing the migration .js files
-    path: path.join(__dirname, '../seeders'),
-    // inject sequelize's QueryInterface in the migrations
-    params: [sequelize.getQueryInterface()],
-  },
-  storage: 'sequelize',
-  storageOptions: {
-    sequelize, // here should be a sequelize instance, not the Sequelize module
-  },
-  logger: console,
-});
 
-(async () => {
-  // Checks migrations and run them if they are not already applied. To keep
-  // track of the executed migrations, a table (and sequelize model) called SequelizeMeta
-  // will be automatically created (if it doesn't exist already) and parsed.
-  try {
-    await migrations.up();
-    await seeders.up();
-    fs.readdirSync(__dirname)
-      .filter((file) => {
-        return (
-          file.indexOf('.') !== 0 &&
-          file !== basename &&
-          file.slice(-3) === '.js'
-        );
-      })
-      .forEach((file) => {
-        const model = require(path.join(__dirname, file))(
-          sequelize,
-          Sequelize.DataTypes,
-        );
-        db[model.name] = model;
-      });
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
+  })
+  .forEach((file) => {
+    try {
+      const model = require(path.join(__dirname, file))(
+        sequelize,
+        Sequelize.DataTypes,
+      );
+      db[model.name] = model;
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
-    Object.keys(db).forEach((modelName) => {
-      if (db[modelName].associate) {
-        db[modelName].associate(db);
-      }
-    });
-  } catch (error) {
-    console.log(error);
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
   }
-})();
-
+});
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
--- public/MainTread.js
+++ public/MainTread.js
@@ -1,17 +1,66 @@
+/* eslint-disable global-require */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable import/no-dynamic-require */
 const path = require('path');
 
 const isDev = require('electron-is-dev');
-const models = isDev
-  ? require('../models')
-  : require(`${path.join(__dirname, '', '../models')}`);
+const fs = require('fs');
+const { Sequelize } = require('sequelize');
+const Umzug = require('umzug');
 
-// Sync Sequelize and the databse
-models.sequelize
-  .sync()
-  .then(() => console.log('Connected'))
-  .catch((error) => console.log(error));
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+
+const config = isDev
+  ? require('../config/config')[env]
+  : require(`${path.join(__dirname, '', '../config/config.js')}`)[env];
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config);
+}
+const migrations = new Umzug({
+  migrations: {
+    // indicates the folder containing the migration .js files
+    path: path.join(__dirname, '../migrations'),
+    // inject sequelize's QueryInterface in the migrations
+    params: [sequelize.getQueryInterface()],
+  },
+  storage: 'sequelize',
+  storageOptions: {
+    sequelize, // here should be a sequelize instance, not the Sequelize module
+  },
+  logger: console,
+});
+const seeders = new Umzug({
+  migrations: {
+    // indicates the folder containing the migration .js files
+    path: path.join(__dirname, '../seeders'),
+    // inject sequelize's QueryInterface in the migrations
+    params: [sequelize.getQueryInterface()],
+  },
+  storage: 'sequelize',
+  storageOptions: {
+    sequelize, // here should be a sequelize instance, not the Sequelize module
+  },
+  logger: console,
+});
+
+(async () => {
+  await migrations.up();
+  await seeders.up();
+  const models = isDev
+    ? require('../models')
+    : require(`${path.join(__dirname, '', '../models')}`);
+
+  // Sync Sequelize and the databse
+  models.sequelize
+    .sync()
+    .then(() => console.log('Connected'))
+    .catch((error) => console.log(error));
+})();
 
 // include User Controller
 isDev
